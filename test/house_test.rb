1bf6812b40bf527a80cc574cedfd7ade8f15b241
DougWelchons <doug.welchons@gmail.com>
2020-12-09 13:33:48 -0700

    Add initialize tests for house class

--- test/house_test.rb
+++ test/house_test.rb
@@ -8,4 +8,23 @@ class HouseTest < Minitest::Test
     house = House.new("$400000", "123 sugar lane")
     assert_instance_of House, house
   end
+
+  def test_it_has_a_price
+    house = House.new("$400000", "123 sugar lane")
+
+    assert_equal "$400000", house.price
+  end
+
+  def test_it_has_an_address
+    house = House.new("$400000", "123 sugar lane")
+
+    assert_equal "123 sugar lane", house.address
+  end
+
+  def test_it_has_no_rooms_by_default
+    house = House.new("$400000", "123 sugar lane")
+
+    assert_equal [], house.rooms
+  end
+
 end
