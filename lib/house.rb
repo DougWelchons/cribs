10f96eb91ab4ae06dd390f589a50eed53e13388e
DougWelchons <doug.welchons@gmail.com>
2020-12-09 14:19:12 -0700

    Add House class passing all current tests

--- lib/house.rb
+++ lib/house.rb
@@ -10,4 +10,32 @@ class House
   def add_room(room)
     @rooms << room
   end
+
+  def price_to_intiger
+    @price.delete('$').to_i
+  end
+
+  def above_market_average?
+    price_to_intiger > 500000
+  end
+
+  def rooms_from_category(type)
+    @rooms.select do |room|
+      room.category == type
+    end
+  end
+
+  def area
+    total_area = 0
+    @rooms.each do |room|
+      total_area += room.area
+    end
+    total_area
+  end
+
+  def details
+    {"price" => price_to_intiger,
+     "address" => address}
+  end
+  
 end
